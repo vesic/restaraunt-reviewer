4df76c12875f960d33113f9c08342759fc623c59
vesic <dusan.vesic@gmail.com>
2016-11-04 13:37:54 +0000

    Update navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,27 @@ import React from 'react'
 
 const NavBar = (props) => {
   return (
-    <nav className="navbar navbar-default">
+    <nav className="navbar navbar-inverse">
       <div className="container-fluid">
         <div className="navbar-header">
-          <a className="navbar-brand" href="#">WebSiteName</a>
+          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#">Restaurant Reviewer</a>
         </div>
-        <ul className="nav navbar-nav">
-          <li className="active"><a href="#">Home</a></li>
-          <li><a href="#">Page 1</a></li>
-          <li><a href="#">Page 2</a></li>
-          <li><a href="#">Page 3</a></li>
-        </ul>
+        <div className="collapse navbar-collapse" id="myNavbar">
+          <ul className="nav navbar-nav">
+            <li className="active"><a href="#">Home</a></li>
+            <li><a href="#">Page 2</a></li>
+            <li><a href="#">Page 3</a></li>
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+            <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
