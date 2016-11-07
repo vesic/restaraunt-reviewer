c8fda91bba3d6dd23f64eb722c98deeed94e8b64
vesic <dusan.vesic@gmail.com>
2016-11-07 10:24:04 +0000

    Fix average

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -6,11 +6,15 @@ const Restaurant = ({setSelectedRestaurant, restaurants}) => {
     <div className="list-group">
     {
       restaurants.map(restaurant => {
+        let sum = 0
+        for (let i = 0; i < restaurant.ratings.starsCount.length; i++) {
+          sum += restaurant.ratings.starsCount[i].stars;
+        }
         return (
           <a key={restaurant._id} href="#" className="list-group-item" onClick={(e) => { e.preventDefault(); setSelectedRestaurant(restaurant);}}>
             <h4 className="list-group-item-heading">{restaurant.name}</h4>
             <div className="list-group-item-text">
-              <AverageRating stars={restaurant.ratings.starsCount.length}/>
+              <AverageRating stars={Math.round(sum / restaurant.ratings.starsCount.length)}/>
             </div>
           </a>
         )
